d8a2dc235249d42a8790c772f5b9fb522a5bb2dd
Andreas Bergmeier <abergmeier@gmx.net>
2013-07-09 19:00:47 +0200

    Add new project type

--- Source/python/project.cpp
+++ Source/python/project.cpp
@@ -3,6 +3,62 @@
 
 using namespace cm::py;
 
+namespace {
+	struct Project : public PyObject {
+	};
+
+	auto Project_type = PyTypeObject{
+		PyVarObject_HEAD_INIT(NULL, 0)
+		"cmake.Project", // tp_name For printing, in format "<module>.<name>"
+		sizeof(Project), // tp_basicsize
+		0                 , // tp_itemsize /* For allocation */
+		nullptr           , // tp_dealloc
+		nullptr           , // tp_print
+		nullptr           , // tp_getattr
+		nullptr           , // tp_setattr
+		nullptr           , // tp_reserved formerly known as tp_compare
+		nullptr           , // tp_repr
+		nullptr           , // tp_as_number
+		nullptr           , // tp_as_sequence
+		nullptr           , // tp_as_mapping
+		nullptr           , // tp_hash
+		nullptr           , // tp_call
+		nullptr           , // tp_str
+		nullptr           , // tp_getattro
+		nullptr           , // tp_setattro
+		nullptr           , // tp_as_buffer
+		Py_TPFLAGS_DEFAULT, // tp_flags
+		"CMake Project objects", // tp_doc Documentation string
+		nullptr , // tp_traverse
+		nullptr , // tp_clear
+		nullptr , // tp_richcompare
+		0       , // tp_weaklistoffset
+		nullptr , // tp_iter
+		nullptr , // tp_iternext
+		nullptr , // tp_methods
+		nullptr , // tp_members
+		nullptr , // tp_getset
+		nullptr , // tp_base;
+		nullptr , // tp_dict
+		nullptr , // tp_descr_get
+		nullptr , // tp_descr_set
+		0       , // tp_dictoffset
+		nullptr , // tp_init
+		nullptr , // tp_alloc
+		nullptr , // tp_new
+		nullptr , // tp_free
+		nullptr , // tp_is_gc
+		nullptr , // tp_bases
+		nullptr , // tp_mro
+		nullptr , // tp_cache
+		nullptr , // tp_subclasses
+		nullptr , // tp_weaklist
+		nullptr , // tp_del
+
+		1 // tp_version_tag
+	};
+}
+
 PyObject*
 create_project( PyObject*, PyObject* args ) {
 
@@ -34,7 +90,10 @@ create_project( PyObject*, PyObject* args ) {
 	cmExecutionStatus status;
 	bool success = cmd.InitialPass( cmdArgs, status );
 
-	return Py_BuildValue("i", success);
+	if( success )
+		return PyObject_New( Project, &Project_type );
+	else
+		return nullptr;
 }
 
 
